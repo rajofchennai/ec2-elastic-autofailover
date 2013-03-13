d86fedf5d9b68e04c782ca893648d071765164a0
rajofchennai <rajofchennai@yahoo.com>
2013-03-13 15:49:31 +0530

    WIP: first cut version of auto failover

--- ec2-elastic-autofailover.gemspec
+++ ec2-elastic-autofailover.gemspec
@@ -4,16 +4,19 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'ec2-elastic-autofailover/version'
 
 Gem::Specification.new do |gem|
-  gem.name          = "ec2-elastic-autofailover"
-  gem.version       = Ec2::Elastic::Autofailover::VERSION
-  gem.authors       = ["rajofchennai"]
-  gem.email         = ["rajofchennai@yahoo.com"]
-  gem.description   = %q{Pings a URL(elastic) if it goes down it shifts elastic ip to another instance}
-  gem.summary       = "Automatic transfer of elastic ip in case of failure"
-  gem.homepage      = ""
-
-  gem.files         = `git ls-files`.split($/)
-  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ["lib"]
+  gem.name           = "ec2-elastic-autofailover"
+  gem.version        = Ec2::Elastic::Autofailover::VERSION
+  gem.authors        = ["rajofchennai"]
+  gem.email          = ["rajofchennai@yahoo.com"]
+  gem.description    = %q{Pings a URL(elastic) if it goes down it shifts elastic ip to another instance}
+  gem.summary        = "Automatic transfer of elastic ip in case of failure"
+  gem.homepage       = ""
+  gem.add_dependency "eventmachine", "~> 1.0.3"
+  gem.add_dependency "rest-client", "~> 1.6.7"
+  gem.add_dependency "amazon-ec2"
+  gem.add_development_dependency "rspec"
+  gem.files          = `git ls-files`.split($/)
+  gem.executables    = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.test_files     = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths  = ["lib"]
 end
--- lib/ec2-elastic-autofailover.rb
+++ lib/ec2-elastic-autofailover.rb
@@ -1,9 +1,63 @@
 require "ec2-elastic-autofailover/version"
+require "AWS"
 
 module Ec2
   module Elastic
-    module Autofailover
+    class Autofailover
       # Your code goes here...
+      def initialize(options = {})
+        # Frequency is specified in seconds
+        @frequency  = options[:frequency] || 30
+        @elastic_ip = options[:elastic_ip]
+        @active     = options[:active_instance_id]
+        # accepts an array
+        @passive    = options[:passive_instance_ids]
+        @url        = options[:url]
+        # support http and https only now
+        @protocol   = options[:protocol] || "http"
+      end
+
+      def start_monitoring
+        EM::PeriodicTime.new(@frequency) {
+          reassign_eip unless ping_url
+        }
+      end
+
+      private
+      def reassign_eip
+        disassociate_eip @active, @elastic_ip
+        new_active = select_passive(@protocol)
+        until associate_eip(new_active, @elastic_ip)
+          new_active = select_passive(@protocol)
+        end
+        @active = new_active
+      end
+
+      def ping_url
+        begin
+          RestClient.get(url)
+        rescue Exception
+          nil
+        end
+      end
+
+      def url
+        "#{@protocol}://#{@url}"
+      end
+
+      def disassociate_eip(instance, eip)
+        Ec2.disassociate_address(:instance_id => instance, :public_ip => eip)
+      end
+
+      def associate_eip(instance, eip)
+        Ec2.associate_address(:instance_id => instance, :public_ip => eip)
+      end
+
+      def select_passive
+        new_active = @passive_instance_ids.sample
+        @passive_instance_ids.delete new_active
+        @passive_instance_ids
+      end
     end
   end
 end
